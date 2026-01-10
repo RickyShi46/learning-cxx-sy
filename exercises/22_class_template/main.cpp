8849a6ce46ae2d2013953e5c77d7293b0bad4993
shiyu7 <shiyu7@lixiang.com>
2026-01-11 01:07:36 +0800

    feat: add subproject validation

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -8,8 +8,14 @@ struct Tensor4D {
     T *data;
 
     Tensor4D(unsigned int const shape_[4], T const *data_) {
-        unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (int i = 0; i < 4; ++i) {
+            shape[i] = shape_[i];
+        }
+        unsigned int size = 1;
+        for (int i = 0; i < 4; ++i) {
+            size *= shape[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -28,6 +34,27 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        for (unsigned int i = 0; i < shape[0]; ++i) {
+            for (unsigned int j = 0; j < shape[1]; ++j) {
+                for (unsigned int k = 0; k < shape[2]; ++k) {
+                    for (unsigned int l = 0; l < shape[3]; ++l) {
+                        // 计算当前张量的线性索引
+                        unsigned int this_idx = ((i * shape[1] + j) * shape[2] + k) * shape[3] + l;
+                        
+                        // 计算广播后的索引（如果 other 维度为1，则使用0，否则使用当前索引）
+                        unsigned int oi = (others.shape[0] == 1) ? 0 : i;
+                        unsigned int oj = (others.shape[1] == 1) ? 0 : j;
+                        unsigned int ok = (others.shape[2] == 1) ? 0 : k;
+                        unsigned int ol = (others.shape[3] == 1) ? 0 : l;
+                        
+                        unsigned int other_idx = ((oi * others.shape[1] + oj) * others.shape[2] + ok) * others.shape[3] + ol;
+                        
+                        // 执行加法
+                        data[this_idx] += others.data[other_idx];
+                    }
+                }
+            }
+        }
         return *this;
     }
 };
